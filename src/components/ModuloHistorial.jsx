b4aad3a1a0bced073f047494f26f550cfd33ae7c
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2026-07-11 16:06:52 -0400

    feat: eliminar importación no utilizada de useAutenticacion en el componente ModuloHistorial

--- src/components/ModuloHistorial.jsx
+++ src/components/ModuloHistorial.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useAutenticacion } from '../context/ContextoAutenticacion';
 import { getReservas } from '../utils/datosSimulados';
 import { 
   Search, 
